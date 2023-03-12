34175d9000bc3d6c279f52e35c56f55a80aa95a8
Sanjai Kumar S <sanjailucifer666@gmail.com>
2023-03-12 15:40:31 +0530

    added some features

---
+++ script.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import Chart from "chart.js/auto";
+
+let leetcode_name = [
+  "sanjai0py",
+  "Pragadesh-45",
+  "sathasivam2001",
+  "Aribaskar_j_b",
+  "PurujitKG",
+];
+
+const ctx = document.getElementById("myChart");
+
+let leetcode = leetcode_name.map((v) =>
+  axios.get(`https://leetcode-stats-api.herokuapp.com/${v}`)
+);
+
+const val = Promise.all(leetcode)
+  .then(function (response) {
+    return response.map((val) => val.data);
+  })
+  .then(function (result) {
+    let totalSolved = result.map((val) => val.totalSolved);
+
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: leetcode_name,
+        datasets: [
+          {
+            label: "# of Solved",
+            data: totalSolved,
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            max: 300,
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  })
+  .catch(function (err) {
+    console.error("Wait for few Minutes");
+  });
